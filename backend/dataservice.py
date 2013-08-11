64329a2e6de8348ededf25c068596b0398e670b0
Javier Rosa <javirosa@manmachinesoftware.com>
2013-08-10 18:45:11 -0700

    db working. TODO send only the current time interval to avgReadings.

--- backend/dataservice.py
+++ backend/dataservice.py
@@ -1,23 +1,27 @@
 from threading import Thread 
-from time import asctime,localtime,sleep,time
+from time import asctime,localtime,sleep,time,mktime,strptime
 from uuid import uuid4
-import csv
-import sys
+from sys import stdout
+import sqlalchemy as sq
+from exceptions import ValueError
+import sqlite3
 class SensorReader(Thread):
 	"""serPort is a pyserial serial port object
+	rowType is a tuple of columnName,type pairs. Implicitly the first entry is "time",int and so avgRow is required to return a type of (int,) + rowType. Valid types are float,int,str,bytes. For strings using a db without arbitrary length strings use bytes.
 	rateSec is the data polling rate the maximum rate that will be returned.
-	db is a database object
 	log is a file handle where data will be pushed to.
-	timestamps will be rounded to the nearest minute."""
-	def __init__(self,commParms,sensorName,rowType = (), rateSec=60,sessionID = None,db=None,log=None):
+	db is an sqlalchemy database engine connection. See: http://docs.sqlalchemy.org/en/rel_0_5/sqlexpression.html and http://docs.sqlalchemy.org/en/rel_0_8/core/connections.html
+	"""
+	def __init__(self,commParms,sensorName,rowType = (),rateSec=60,sessionID = None,db=None,log=None):
 		Thread.__init__(self)
 		self.setName(sensorName)
 		self.commParms = commParms 
 		self.rateSec = rateSec
 		self.db = db
 		self.log = log
-		self.dbTable = sensorName
+		self.sensorName = sensorName
 		self.end = False
+		self.rowType = (("time",int),) + rowType
 		if not sessionID:
 			self.sessionID = uuid4()
 		else:
@@ -27,20 +31,24 @@ class SensorReader(Thread):
 		while(not self.end):
 			if localtime().tm_sec%self.rateSec == 0:
 				readings = self.getReadings()
-				readingsStr = ["\"%s\""%str(reading) for reading in readings]
+				readingsStr = ["\"%s\""%repr(reading) for reading in readings]
 				rows = [self.toRow(reading) for reading in readings]
-				avgRow = self.avgRows(rows)
+				avgRows = self.avgRows(rows)
 				timeStamp = asctime(localtime())
 				logEntry = ",".join(
-						[str(self.sessionID),timeStamp, "\"%s\""%str(avgRow)] + readingsStr) +"\n"
+						[str(self.sessionID),timeStamp, "\"%s\""%repr(avgRows)] + readingsStr) +"\n"
 				if self.log:
 						  self.log.write(logEntry)
+				stdout.flush()
+				for row in avgRows:
+					self.dbTable.insert().values(*zip(row,zip(*self.rowType)[0]))
 				wait = self.rateSec - time()%self.rateSec
 				sleep(wait)
 			sleep(.1)
 
-	"""Initialized the sensor using initSensor and starts the data collection loop"""
+	"""Initialized the sensor using initSensor and starts the data collection loop."""
 	def startCollection(self):
+		self.initDB()
 		self.end = False
 		self.initSensor()
 		self.start()
@@ -53,9 +61,27 @@ class SensorReader(Thread):
 		if self.log:
 			self.log.close()
 
+	def toDBType(self,ty):
+		if ty == int:
+			return sq.Integer
+		if ty == float:
+			return sq.Float
+		if ty == str:
+			return sq.String
+		if ty == bytes or ty == bytearray:
+			return sq.Binary
+		raise ValueError("Does not map to a database type.")
 	def initDB(self):
-		#create table
-		pass
+		if not self.db: return
+		cols = []
+		cols.append(sq.Column('time',self.toDBType(int),primary_key = True))
+		print self.rowType
+		for name,ty in self.rowType[1:]:
+			cols.append(sq.Column(name,self.toDBType(ty)))
+		metadata = sq.MetaData(bind=self.db)
+		
+		self.dbTable = sq.Table(self.sensorName,metadata,*cols)
+		self.dbTable.create()
 
 	"""Called by startCollection to initialize the sensor and communcations."""
 	def initSensor(self):
@@ -63,22 +89,23 @@ class SensorReader(Thread):
 	"""Called by stop after collection has ended."""
 	def stopSensor(self):
 		pass
-	"""Returns raw readings gather from sensor. Later on will be converted to rows by a call to toRow."""
+	"""Returns raw readings gather from sensor. Later on will be converted to rows by a call to toRow. Must return a list of readings."""
 	def getReadings(self):
-		return []
+		return [asctime(localtime())]
 	"""Must return a tuple following the type of rowType in __init__"""
 	def avgRows(self, rows):
 		if len(rows) > 0:
-			return rows[0]
+			return [rows[0]]
  		else:
 			return None
 	"""Takes the raw sensor output and converts it to a row. A collection of these will be sent to avgRows."""
 	def toRow(self,reading):
-		return ()
+		row = strptime(reading)
+		return (int(mktime(row)),)
 		
 class SerialSensorReader(SensorReader):
-	def __init__(self,rowType = ()):
-		SensorReader.__init__(self)
+	def __init__(self,commParms,sensorName,rowType = (),rateSec=60,sessionID = None,db=None,log=None):
+		SensorReader.__init__(self,commParms,sensorName,rowType,rateSec,sessionID,db,log)
 	def initSensor(self):
 		from serial import Serial
 		self.serialP = Serial(**self.commParms) 
@@ -95,7 +122,8 @@ class SerialSensorReader(SensorReader):
 
 if __name__ == "__main__":
 	try:
-		sr = SensorReader("COM1","Test",(),1,None,None,sys.stdout)
+		db = sq.create_engine('sqlite:///:memory:', echo=True)
+		sr = SensorReader("COM1","Test2",(),1,None,db,stdout)
 		sr.startCollection()
 		sleep(100)
 		sr.stop()
@@ -105,5 +133,6 @@ if __name__ == "__main__":
 
 #todo
 #add database query
-#add tuples to define row
-#automate avg function?
+#fix it so that avgRow only deals with one averaged collection of readings per time interval
+#getReadings returns all readings with a timestamp for each?
+#then we only return the readings for one time interval?
