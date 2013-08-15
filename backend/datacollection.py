f7ad8f52384c530fa7f8666ce755c30140fb5b79
Javier Rosa <javirosa@manmachinesoftware.com>
2013-08-14 23:28:26 -0700

    Fixed some errors in datacollection.py need to fix a few more before it will work.

--- backend/datacollection.py
+++ backend/datacollection.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 from ConfigParser import SafeConfigParser
-from os import path,mkdir
+from os import path,mkdir,makedirs
 import time
 import sqlalchemy
+from sys import stdout
+from dataservice import SerialSensorReader
 #In configuration file have sensors and associate log filenames
 #Have list of active sensors and directories for reading files.
 #After reading a sensor file move it to a backup directory
-CFGFILE="./backend.cfg"
+CFGFILE="backend.cfg"
 
-logdir="./logs"
+logdir="logs"
 session_id="noid"
 
 def gettimestamp():
-	return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+	return time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime())
 def mklogfile(sensorname):
-	return file(path.join(logdir,sensorname+gettimestamp()+".csv"))
+	logpath = path.join(logdir,sensorname+gettimestamp()+".csv")
+	return open(logpath,'w+')
 def main():
+	global logdir
 	if path.exists(CFGFILE):
 		cfg=SafeConfigParser()
 		cfg.read(CFGFILE)
@@ -23,7 +27,9 @@ def main():
 		print "ConfigFile not found."
 		exit()
 	logdir = cfg.get("global","logdir")
-	makedirs(logdir)
+	if not path.exists(logdir):
+		print "Creating: \'%s\'"%logdir
+		makedirs(logdir)
 	session_id = cfg.get("global","session_id")
 	sensors_present = dict([(name,bool(x)) for name,x in cfg.items("sensors_present")])
 	sensors_present.setdefault(False)
@@ -35,7 +41,16 @@ def main():
 		commConfig = {'port':cfg.get(curSensor,'port'),'timeout':cfg.getint('global','timeout'),
 				'bytesize':7,'parity':'N'}
 		logFile = mklogfile(curSensor)
-		rowType = ('aeth_date':str,'aeth_time':str,'black_carbon':int,'a':float,'b':float,'c':float,'d':float,'e':float,'f':float,'g':float)
+		rowType = (('aeth_date',str),
+				('aeth_time',str),
+				('black_carbon',int),
+				('a',float),
+				('b',float),
+				('c',float),
+				('d',float),
+				('e',float),
+				('f',float),
+				('g',float))
 		sensors[curSensor] = SerialSensorReader(commConfig=commConfig,sensorName=curSensor,rowType=rowType,rateSec=60,db=None,log=logFile)
 		print '%s online'%curSensor
 	if sensors_present.get('dustrack'):
@@ -44,8 +59,8 @@ def main():
 	if sensors_present.get(curSensor):
 		commConfig = {'port':cfg.get(curSensor,'port'),'timeout':cfg.getint('global','timeout')}
 		logFile = mklogfile(curSensor)
-		rowType = ('bscat_m_1':float,'calibcoef':float,'preassure_mb':int,'temp_K':int,'RH_percent':int,'relay_status':int)
-		sensors[curSensor] = SerialSensorReader(commConfig=commConfig,sensorName=cursensor,rowType=rowType,rateSec=60,db=None,log=logFile)
+		rowType = (('bscat_m_1',float),('calibcoef',float),('preassure_mb',int),('temp_K',int),('RH_percent',int),('relay_status',int))
+		sensors[curSensor] = SerialSensorReader(commConfig=commConfig,sensorName=curSensor,rowType=rowType,rateSec=60,db=None,log=logFile)
 		print '%s online'%curSensor
 	if sensors_present.get('metone'):
 		print 'metone online'
@@ -53,6 +68,14 @@ def main():
 		print 'ucpc online'
 	if sensors_present.get('vueiss'):
 		print 'vueiss online'
+
+	print sensors
+	for key,value in sensors.iteritems():
+		value.start()
+	sleep(10)
+
+	for sensor in sensors:
+		sensor.stop()
 
 if __name__ == "__main__":
 	main()
